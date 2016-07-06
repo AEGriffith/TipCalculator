1e5e1ad276e765997c512f9a0f18f08ef0c7a329
Amanda Griffith <amandaegriffith@gmail.com>
2016-07-06 13:17:26 -0500

    Finished part 3 of tutorial

--- TipCalculatorModel.swift
+++ TipCalculatorModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class TipCalculatorModel {
-
+    
     var total: Double
     var taxPct: Double
     var subtotal: Double {
@@ -9,27 +9,29 @@ class TipCalculatorModel {
             return total / (taxPct + 1)
         }
     }
-
+    
     init(total: Double, taxPct: Double) {
         self.total = total
         self.taxPct = taxPct
     }
-
-    func calcTipWithTipPct(tipPct: Double) -> Double {
-        return subtotal * tipPct
+    
+    func calcTipWithTipPct(tipPct: Double) -> (tipAmt: Double, total: Double) {
+        let tipAmt = subtotal * tipPct
+        let finalTotal = total + tipAmt
+        return (tipAmt, finalTotal)
     }
-
-    func returnPossibleTips() -> [Int: Double] {
-
+    
+    func returnPossibleTips() -> [Int: (tipAmt: Double, total: Double)] {
+        
         let possibleTipsInferred = [0.15, 0.18, 0.20]
-
-        var retval = [Int: Double]()
+        
+        var retval = [Int: (tipAmt:Double, total:Double)]()
         for possibleTip in possibleTipsInferred {
             let intPct = Int(possibleTip*100)
             retval[intPct] = calcTipWithTipPct(possibleTip)
         }
         return retval
-
+        
     }
-
+    
 }
